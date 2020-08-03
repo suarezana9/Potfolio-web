85fa9c6744889d260c3d58ec43b90aee7a7338f0
suarezana9 <suarezsuarezana20@gmail.com>
2020-08-03 10:46:29 -0300

    button download cv ready, projects with images realies

--- src/app/image/shared/image.service.ts
+++ src/app/image/shared/image.service.ts
@@ -12,10 +12,10 @@ export class ImageService {
     } 
 }
 const IMAGES=[
-    {"id":1, "category":"Design", "caption":"Single page Portfolio", "nameClient":"Luigi Gutierrez", "date":"23-03-2018", "liveDemo":"www.website.com", "status":"completed", "description": "Design and development of a portfolio for a web developer. under html, css, javascript, angular, node.js, express and mongoDB technologies.", "url": "../assets/img/proyecto1.jpeg"},
+    {"id":1, "category":"Design", "caption":"TechFit", "nameClient":"Luigi Gutierrez", "date":"23-03-2019", "liveDemo":"https://github.com/suarezana9", "status":"In progress", "description": "Web page design and development for a gym for healthy training and eating, using html, css, javascript, jquery technologies.", "url": "../assets/img/techfit.png"},
 
     {"id":2, "category":"Development", "caption":"Tempo Sushi", "nameClient":"Carol Bautista", "date":"20-06-2020", "liveDemo":"www.website.com", "status":"In progress", "description":"This project is inspired by a website for a Sushi business, with an exclusive design and adapted to customer needs. Here stands out the friendly design to publicize your products",  "url": "../assets/img/proyecto2.1.jpeg"},
 
-    {"id":3, "category":"Development", "caption":"Online store", "nameClient":"Patricia Uribe", "date":"15-01-2020", "liveDemo":"www.website.com", "status":"In progress", "description": "building a personal blog using html and css technologies. A design was implemented under customer expectations", "url": "../assets/img/proyecto3.jpeg"},
+    {"id":3, "category":"Development", "caption":"Online store", "nameClient":"Patricia Uribe", "date":"15-01-2020", "liveDemo":"www.website.com", "status":"In progress", "description": "website development for an accessories and underwear store, designed under customer requirements and a good user experience. This site is developed with html, css, javascript and bootstrap", "url": "../assets/img/proyecto3.jpeg"},
     
 ]
